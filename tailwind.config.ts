9552b7c8ba8a9984851103ff8e7e54c354859c1a
Hitekk47 <54647949+Hitekk47@users.noreply.github.com>
2025-11-23 18:04:19 +0000

    fix tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          dark: '#0f172a',
+          primary: '#2563eb',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
